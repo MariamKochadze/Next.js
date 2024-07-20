5f28a4c3a40e45e5d6958a866da9af917bd6f27d
MariamKochadze <kochadzemariam80@gmail.com>
2024-07-20 19:06:21 +0400

    add loader tsx file used in promise only with contact us page

--- app/routing/contactus/page.tsx
+++ app/routing/contactus/page.tsx
@@ -1,7 +1,18 @@
 'use client'
 
+const newPromise = new Promise ((res,rej)=>{
+    setTimeout(()=>{
+      res('Contact us page will be added soon ')
+    },5000)
+})
+
+
 export default function contactus(){
+    const promise = newPromise;
     return(
+        <>
+        {promise}
         <div>Contact Us</div>
+        </>
     )
 }
